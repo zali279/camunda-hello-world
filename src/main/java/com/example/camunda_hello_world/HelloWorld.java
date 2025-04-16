22a60e4e1bdc8984a5187de406deafca77a60c79
Ali Aladraj <ali.aladraj@nbbonline.com>
2025-04-16 11:15:29 +0300

    write execute method

--- src/main/java/com/example/camunda_hello_world/HelloWorld.java
+++ src/main/java/com/example/camunda_hello_world/HelloWorld.java
@@ -1,8 +1,11 @@
 package com.example.camunda_hello_world;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+@Component("helloWorld")
 public class HelloWorld implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
