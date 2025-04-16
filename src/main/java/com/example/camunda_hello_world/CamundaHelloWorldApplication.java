612aa97acc4dcdf7fd5fa8bfdff4d8535d4afb5d
Ali Aladraj <ali.aladraj@nbbonline.com>
2025-04-16 08:40:55 +0300

    Initial commit - Spring Boot + Camunda Hello World

---
+++ src/main/java/com/example/camunda_hello_world/CamundaHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camunda_hello_world/CamundaHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camunda_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
