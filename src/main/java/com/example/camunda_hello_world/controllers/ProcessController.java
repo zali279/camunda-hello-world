e9d0d26dcca7dfd6c137702218a66ecdffabb6f9
zali279 <zahraaalihasan279@gmail.com>
2025-04-17 08:41:55 +0300

    remove @Autowired and add lombok

--- src/main/java/com/example/camunda_hello_world/controllers/ProcessController.java
+++ src/main/java/com/example/camunda_hello_world/controllers/ProcessController.java
@@ -1,17 +1,18 @@
 package com.example.camunda_hello_world.controllers;
 
+import lombok.AllArgsConstructor;
 import org.camunda.bpm.engine.RuntimeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/start")
 public class ProcessController {
 
-    @Autowired
-    private RuntimeService runtimeService;
+    private final RuntimeService runtimeService;
 
     @PostMapping("")
     public String startHelloWorld() {
