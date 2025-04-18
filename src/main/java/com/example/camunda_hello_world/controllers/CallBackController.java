0552f6ce0f6586797e5e97d829f037216696a63a
zahra <zahraaalihasan279@gmail.com>
2025-04-18 16:00:36 +0300

    create callback end-point / controller

---
+++ src/main/java/com/example/camunda_hello_world/controllers/CallBackController.java
@@ -0,0 +1,25 @@
+package com.example.camunda_hello_world.controllers;
+
+
+import lombok.AllArgsConstructor;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/callback")
+@AllArgsConstructor
+public class CallBackController {
+    final private RuntimeService runtimeService;
+
+    @PostMapping("")
+    public  String callBack(){
+//        runtimeService.createMessageCorrelation("Message_3qe8ssh").processInstanceBusinessKey("test").correlate();
+        runtimeService.createMessageCorrelation("callbackReceived").correlate();
+        return "callback ...";
+    }
+
+
+
+}
